469b55263a095c608a67e489e6a6f0b94ab1c7e3
Matija Jaic <jaicmatija@gmail.com>
2019-05-07 15:50:23 +0200

    Fix credentials

--- db/connect.js
+++ db/connect.js
@@ -1,16 +1,17 @@
-const { Pool } = require('pg');
+const {
+  Pool,
+} = require('pg');
 
 const pool = new Pool({
-  host: '138.68.87.73',
   user: 'matijaja',
+  host: '138.68.87.73',
   password: 'TDd7#r7Z',
   database: 'db_matijaja',
   port: 5432,
-})
+});
 
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
   },
-}
-
+};
